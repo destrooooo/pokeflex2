5076ed2d73cf69ba5da853fe0a01f94b76bbaa23
destrooooo <clementsilva@live.fr>
2024-03-08 11:33:34 +0100

    Basic setup
    pushing things we've already done accross the week

---
+++ src/TypeTable.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+
+const dataToPass = [
+  {
+    type_id: 1,
+    type_name: "Normal",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/8/89/NormalIC_BDSP.png/105px-NormalIC_BDSP.png",
+  },
+  {
+    type_id: 2,
+    type_name: "Fighting",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/3/35/FightingIC_BDSP.png/105px-FightingIC_BDSP.png",
+  },
+  {
+    type_id: 3,
+    type_name: "Flying",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/0/03/FlyingIC_BDSP.png/105px-FlyingIC_BDSP.png",
+  },
+  {
+    type_id: 4,
+    type_name: "Poison",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/8/8b/PoisonIC_BDSP.png/105px-PoisonIC_BDSP.png",
+  },
+  {
+    type_id: 5,
+    type_name: "Ground",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/b/bd/GroundIC_BDSP.png/105px-GroundIC_BDSP.png",
+  },
+  {
+    type_id: 6,
+    type_name: "Rock",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/e/e6/RockIC.png/105px-RockIC.png",
+  },
+  {
+    type_id: 7,
+    type_name: "Bug",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/9/9c/BugIC_BDSP.png/105px-BugIC_BDSP.png",
+  },
+  {
+    type_id: 8,
+    type_name: "Ghost",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/4/45/GhostIC_BDSP.png/105px-GhostIC_BDSP.png",
+  },
+  {
+    type_id: 9,
+    type_name: "Steel",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/5/5f/SteelIC_BDSP.png/105px-SteelIC_BDSP.png",
+  },
+  {
+    type_id: 10,
+    type_name: "Fire",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/b/b1/FireIC_BDSP.png/105px-FireIC_BDSP.png",
+  },
+  {
+    type_id: 11,
+    type_name: "Water",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/2/2b/WaterIC_BDSP.png/105px-WaterIC_BDSP.png",
+  },
+  {
+    type_id: 12,
+    type_name: "Grass",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/a/ad/GrassIC_BDSP.png/105px-GrassIC_BDSP.png",
+  },
+  {
+    type_id: 13,
+    type_name: "Electric",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/c/c1/ElectricIC_BDSP.png/105px-ElectricIC_BDSP.png",
+  },
+  {
+    type_id: 14,
+    type_name: "Psychic",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/6/68/PsychicIC_BDSP.png/105px-PsychicIC_BDSP.png",
+  },
+  {
+    type_id: 15,
+    type_name: "Ice",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/1/11/IceIC_BDSP.png/105px-IceIC_BDSP.png",
+  },
+  {
+    type_id: 16,
+    type_name: "Dragon",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/c/c7/DragonIC_BDSP.png/105px-DragonIC_BDSP.png",
+  },
+  {
+    type_id: 17,
+    type_name: "Dark",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/b/b9/DarkIC_BDSP.png/105px-DarkIC_BDSP.png",
+  },
+  {
+    type_id: 18,
+    type_name: "Fairy",
+    type_url:
+      "https://archives.bulbagarden.net/media/upload/thumb/d/d6/FairyIC_BDSP.png/105px-FairyIC_BDSP.png",
+  },
+];
